dbea6407d0242bd8ca91e09897902c7f9326fc1f
takanori-matsushita <t.matsushita@arcjobz.com>
2021-04-15 00:04:16 +0900

    add login register page

--- pages/login.vue
+++ pages/login.vue
@@ -4,40 +4,45 @@
       <h1 class="font-bold text-center text-2xl mb-5">nuxt-firebase-auth</h1>
       <div class="bg-white shadow w-full rounded-lg divide-y divide-gray-200">
         <div class="px-5 py-5">
-          <label class="font-semibold text-sm text-gray-600 pb-1 block"
-            >E-mail</label
-          >
-          <input
-            type="text"
-            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-          />
-          <label class="font-semibold text-sm text-gray-600 pb-1 block"
-            >Password</label
-          >
-          <input
-            type="text"
-            class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
-          />
-          <button
-            type="button"
-            class="mb-3 transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
-          >
-            <span class="inline-block mr-2">Login</span>
-            <svg
-              xmlns="http://www.w3.org/2000/svg"
-              fill="none"
-              viewBox="0 0 24 24"
-              stroke="currentColor"
-              class="w-4 h-4 inline-block"
+          <form @submit.prevent="login()">
+            <label class="font-semibold text-sm text-gray-600 pb-1 block"
+              >E-mail</label
             >
-              <path
-                stroke-linecap="round"
-                stroke-linejoin="round"
-                stroke-width="2"
-                d="M17 8l4 4m0 0l-4 4m4-4H3"
-              />
-            </svg>
-          </button>
+            <input
+              v-model="form.email"
+              type="text"
+              class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+            />
+            <label class="font-semibold text-sm text-gray-600 pb-1 block"
+              >Password</label
+            >
+            <input
+              v-model="form.password"
+              type="password"
+              class="border rounded-lg px-3 py-2 mt-1 mb-5 text-sm w-full"
+            />
+            <button
+              type="submit"
+              class="mb-3 transition duration-200 bg-blue-500 hover:bg-blue-600 focus:bg-blue-700 focus:shadow-sm focus:ring-4 focus:ring-blue-500 focus:ring-opacity-50 text-white w-full py-2 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
+            >
+              <span class="inline-block mr-2">Login</span>
+              <svg
+                xmlns="http://www.w3.org/2000/svg"
+                fill="none"
+                viewBox="0 0 24 24"
+                stroke="currentColor"
+                class="w-4 h-4 inline-block"
+              >
+                <path
+                  stroke-linecap="round"
+                  stroke-linejoin="round"
+                  stroke-width="2"
+                  d="M17 8l4 4m0 0l-4 4m4-4H3"
+                />
+              </svg>
+            </button>
+          </form>
+
           <nuxt-link
             class="mb-10 transition duration-200 bg-green-500 hover:bg-green-600 focus:bg-green-700 focus:shadow-sm focus:ring-4 focus:ring-green-500 focus:ring-opacity-50 text-white w-full py-2 rounded-lg text-sm shadow-sm hover:shadow-md font-semibold text-center inline-block"
             to="/register"
@@ -63,7 +68,25 @@
 </template>
 
 <script>
-export default {}
+export default {
+  data() {
+    return {
+      form: {},
+    }
+  },
+  methods: {
+    async login() {
+      try {
+        const [email, password] = [this.form.email, this.form.password]
+        await this.$firebase.auth().signInWithEmailAndPassword(email, password)
+        this.$router.push('/')
+      } catch (e) {
+        console.error(e.message)
+        alert('ログイン失敗')
+      }
+    },
+  },
+}
 </script>
 
 <style></style>
